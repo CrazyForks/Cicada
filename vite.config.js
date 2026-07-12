c0c237f269015a64c5e5b19457128f231182a180
Ansel <238927804+asong56@users.noreply.github.com>
2026-07-11 21:31:48 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -11,30 +10,6 @@ export default defineConfig({
             removeComments: true,
             minifyCSS: true,
             minifyJS: true,
-        }),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'Cicada Whiteboard',
-                short_name: 'Cicada',
-                icons: [
-                    {
-                        src: 'assets/web-app-manifest-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                        "purpose": "maskable"
-                    },
-                    {
-                        src: 'assets/web-app-manifest-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        "purpose": "maskable"
-                    }
-                ],
-                "theme_color": "#f2f2f7",
-                "background_color": "#f2f2f7",
-                "display": "standalone"
-            }
         })
     ],
     build: {
